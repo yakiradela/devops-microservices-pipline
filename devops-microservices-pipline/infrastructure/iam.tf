84400c83e631544b6f42c2aa6b898985f6a133f9
Yakir Adela <yakirade@gmail.com>
2025-05-19 13:08:52 +0300

    Update iam.tf

--- devops-microservices-pipline/infrastructure/iam.tf
+++ devops-microservices-pipline/infrastructure/iam.tf
@@ -1,45 +1,73 @@
-# יוצרים את ה-Role עבור הקלאסטר של EKS
+# ===== Cluster Role =====
 resource "aws_iam_role" "eks_cluster_role" {
   name = "eks-cluster-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
-    Statement: [
-      {
-        Effect = "Allow",
-        Principal = {
-          Service = "eks.amazonaws.com"
-        },
-        Action = "sts:AssumeRole"
-      }
-    ]
+    Statement: [{
+      Effect = "Allow",
+      Principal = {
+        Service = "eks.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
   })
 }
 
-# מצרפים את המדיניות שמאפשרת לקלאסטר של EKS לעבוד
-resource "aws_iam_role_policy_attachment" "eks_cluster_policy_attachment" {
+resource "aws_iam_role_policy_attachment" "eks_cluster_attach" {
   role       = aws_iam_role.eks_cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
-# (אופציונלי - רק אם אתה מנהל את המשתמש דרך Terraform) 
-# מעניקים למשתמש yakiradela את ההרשאה לבצע PassRole על ה-Role שנוצר
-resource "aws_iam_policy" "passrole_policy" {
-  name        = "AllowPassRoleToEKSCluster"
-  description = "Allows user yakiradela to pass the eks cluster role"
-  policy      = jsonencode({
+# ===== Worker Node Role =====
+resource "aws_iam_role" "worker_node_role" {
+  name = "eks-worker-node-role"
+
+  assume_role_policy = jsonencode({
     Version = "2012-10-17",
-    Statement: [
-      {
-        Effect = "Allow",
-        Action = "iam:PassRole",
-        Resource = aws_iam_role.eks_cluster_role.arn
-      }
-    ]
+    Statement: [{
+      Effect = "Allow",
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
   })
 }
 
-resource "aws_iam_user_policy_attachment" "attach_passrole_policy" {
+resource "aws_iam_role_policy_attachment" "worker_node_AmazonEKSWorkerNodePolicy" {
+  role       = aws_iam_role.worker_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "worker_node_AmazonEC2ContainerRegistryReadOnly" {
+  role       = aws_iam_role.worker_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+resource "aws_iam_role_policy_attachment" "worker_node_AmazonEKS_CNI_Policy" {
+  role       = aws_iam_role.worker_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+# ===== Allow PassRole to current user =====
+resource "aws_iam_policy" "passrole_policy" {
+  name        = "AllowPassRoleToEKS"
+  description = "Allow user to pass cluster role to EKS"
+  policy      = jsonencode({
+    Version = "2012-10-17",
+    Statement: [{
+      Effect = "Allow",
+      Action = "iam:PassRole",
+      Resource = [
+        aws_iam_role.eks_cluster_role.arn,
+        aws_iam_role.worker_node_role.arn
+      ]
+    }]
+  })
+}
+
+resource "aws_iam_user_policy_attachment" "user_passrole_attach" {
   user       = "yakiradela"
   policy_arn = aws_iam_policy.passrole_policy.arn
 }
