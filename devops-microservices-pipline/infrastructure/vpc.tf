ffb2d9a9ee6ab29796641ea38bd6b5950e18c1af
Yakir Adela <yakirade@gmail.com>
2025-05-22 10:03:07 +0300

    Update vpc.tf

--- devops-microservices-pipline/infrastructure/vpc.tf
+++ devops-microservices-pipline/infrastructure/vpc.tf
@@ -13,3 +13,29 @@ resource "aws_subnet" "public_2" {
   cidr_block        = "10.0.2.0/24"
   availability_zone = "us-east-2b"
 }
+
+# 🆕 Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+
+# 🆕 Route Table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+# 🆕 Associate route table with subnets
+resource "aws_route_table_association" "public_1" {
+  subnet_id      = aws_subnet.public_1.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_2" {
+  subnet_id      = aws_subnet.public_2.id
+  route_table_id = aws_route_table.public.id
+}
