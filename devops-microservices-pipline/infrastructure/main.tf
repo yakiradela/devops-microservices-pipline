b0ee6afd3b7c46c6990f722d9e89cf659199edc6
Yakir Adela <yakirade@gmail.com>
2025-05-22 08:47:10 +0300

    Update main.tf

--- devops-microservices-pipline/infrastructure/main.tf
+++ devops-microservices-pipline/infrastructure/main.tf
@@ -7,7 +7,10 @@ resource "aws_eks_cluster" "main" {
   role_arn = aws_iam_role.eks_cluster_role.arn
 
   vpc_config {
-    subnet_ids = var.subnet_ids
+    subnet_ids = [
+      aws_subnet.public_1.id,
+      aws_subnet.public_2.id
+    ]
   }
 }
 
