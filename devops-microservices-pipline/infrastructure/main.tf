7b6eaaf3c4600a463c79e357b6ce81b6d01a8b67
Yakir Adela <yakirade@gmail.com>
2025-05-22 12:13:35 +0300

    Update main.tf

--- devops-microservices-pipline/infrastructure/main.tf
+++ devops-microservices-pipline/infrastructure/main.tf
@@ -19,6 +19,7 @@ resource "aws_eks_cluster" "main" {
 resource "aws_eks_node_group" "default" {
   cluster_name    = aws_eks_cluster.main.name
   node_group_name = "default"
+  node_role_arn   = aws_iam_role.worker_node_role.arn
   subnet_ids = [
     aws_subnet.public_1.id,
     aws_subnet.public_2.id
