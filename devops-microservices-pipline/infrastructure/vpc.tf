1a2c3424db4ffd619b369577ba68543fd3618d0b
Yakir Adela <yakirade@gmail.com>
2025-05-22 09:00:37 +0300

    Create vpc.tf

---
+++ devops-microservices-pipline/infrastructure/vpc.tf
@@ -0,0 +1,15 @@
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "public_1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-2a"
+}
+
+resource "aws_subnet" "public_2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-2b"
+}
