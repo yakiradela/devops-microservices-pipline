874df5a646aa3b3af4eea2e448c89237ee7d4818
Yakir Adela <yakirade@gmail.com>
2025-05-22 12:06:56 +0300

    Update main.tf

--- devops-microservices-pipline/infrastructure/main.tf
+++ devops-microservices-pipline/infrastructure/main.tf
@@ -5,7 +5,6 @@ provider "aws" {
 # EKS Cluster
 resource "aws_eks_cluster" "main" {
   name     = var.cluster_name
-  role_arn = aws_iam_role.eks_cluster_role.arn
 
   vpc_config {
     subnet_ids = [
@@ -19,7 +18,6 @@ resource "aws_eks_cluster" "main" {
 resource "aws_eks_node_group" "default" {
   cluster_name    = aws_eks_cluster.main.name
   node_group_name = "default"
-  node_role_arn   = aws_iam_role.worker_node_role.arn
   subnet_ids = [
     aws_subnet.public_1.id,
     aws_subnet.public_2.id
