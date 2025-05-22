6b083c855ec1b998f313ebffb584b9ef5bb7cdd4
Yakir Adela <yakirade@gmail.com>
2025-05-22 12:10:20 +0300

    Update main.tf

--- devops-microservices-pipline/infrastructure/main.tf
+++ devops-microservices-pipline/infrastructure/main.tf
@@ -5,6 +5,7 @@ provider "aws" {
 # EKS Cluster
 resource "aws_eks_cluster" "main" {
   name     = var.cluster_name
+  role_arn = aws_iam_role.eks_cluster_role.arn
 
   vpc_config {
     subnet_ids = [
